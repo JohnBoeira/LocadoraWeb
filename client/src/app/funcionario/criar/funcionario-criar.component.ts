69835e62794cdc1fe5393562bedd3c752322d1d1
johnboeira <jbiel72001@gmail.com>
2021-11-17 22:33:52 -0300

    iniciando funcionário

---
+++ client/src/app/funcionario/criar/funcionario-criar.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-funcionario-criar',
+  templateUrl: './funcionario-criar.component.html',
+})
+export class FuncionarioCriarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ client/src/app/funcionario/editar/funcionario-editar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-funcionario-editar',
+  templateUrl: './funcionario-editar.component.html',
+ 
+})
+export class FuncionarioEditarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
