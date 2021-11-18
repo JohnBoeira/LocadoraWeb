400e928a21fbf7accf51d7b5d20664dd853d4233
johnboeira <jbiel72001@gmail.com>
2021-11-18 15:05:53 -0300

    Modificações

--- client/src/app/funcionario/listar/funcionario-listar.component.ts
+++ client/src/app/funcionario/listar/funcionario-listar.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-funcionario-listar',
   templateUrl: './funcionario-listar.component.html',
-  styleUrls: ['./funcionario-listar.component.css']
+
 })
 export class FuncionarioListarComponent implements OnInit {
 
---
+++ server/src/LocadoraVeiculos.WebApi/ViewModels/FuncionarioViewModel.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LocadoraVeiculos.WebApi.ViewModels
+{
+    public class FuncionarioListViewModel
+    {
+        public int Id { get; set; }
+
+        public string Nome { get; set; }
+
+        public string Usuario { get; set; }
+    }
+
+    public class FuncionarioDetailsViewModel
+    {
+        public int Id { get; set; }
+
+        public string Nome { get; set; }
+
+        public string Usuario { get; set; }
+
+        public string Senha { get; set; }
+
+        public decimal Salario { get; set; }
+
+        public DateTime DataAdmissao { get; set; }
+
+    }
+
+    public class FuncionarioCreateViewModel
+    {
+
+        public string Nome { get; set; }
+
+        public string Usuario { get; set; }
+
+        public string Senha { get; set; }
+
+        public decimal Salario { get; set; }
+
+        public DateTime DataAdmissao { get; set; }
+    }
+
+    public class FuncionarioEditViewModel
+    {
+        public int Id { get; set; }
+
+        public string Nome { get; set; }
+
+        public string Usuario { get; set; }
+
+        public string Senha { get; set; }
+
+        public decimal Salario { get; set; }
+
+        public DateTime DataAdmissao { get; set; }
+    }
+}
