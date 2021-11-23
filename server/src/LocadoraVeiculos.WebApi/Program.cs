fcefdf4506fe73818b0002beb4c640e263954c4d
JohnBoeira <johnboeira@gmail.com>
2021-11-23 11:38:33 -0300

    teste

--- server/src/LocadoraVeiculos.WebApi/Program.cs
+++ server/src/LocadoraVeiculos.WebApi/Program.cs
@@ -16,7 +16,7 @@ namespace LocadoraVeiculos.WebApi
         {
             CreateHostBuilder(args).Build().Run();
         }
-
+         
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
             .UseServiceProviderFactory(new AutofacServiceProviderFactory())
